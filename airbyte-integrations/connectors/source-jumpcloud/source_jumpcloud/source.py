b3706ac7ff5feae2bf31666e7fbd7fb7e6487549
Enrico Tuvera Jr <enrico.tuvera@infinit-o.com>
2022-01-28 18:31:29 +0800

    updatadded openapi spec, systemusers.json spec

--- airbyte-integrations/connectors/source-jumpcloud/source_jumpcloud/source.py
+++ airbyte-integrations/connectors/source-jumpcloud/source_jumpcloud/source.py
@@ -45,8 +45,8 @@ class JumpcloudStream(HttpStream, ABC):
         super().__init__(*args, **kwargs)
 
         self.config = config
-        self.total = 0
         self.offset = 0
+        self.limit = 100
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
@@ -61,7 +61,12 @@ class JumpcloudStream(HttpStream, ABC):
         :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
                 If there are no more pages in the result, return None.
         """
-        return None
+        total = response.json().get("total")
+        if self.offset < total:
+            self.offset = self.offset + self.limit
+            return {'skip': self.offset}
+        else:
+            return {'skip': self.offset} 
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
@@ -72,7 +77,10 @@ class JumpcloudStream(HttpStream, ABC):
 
         :return a dictionary
         """
-        return {}
+        if next_page_token:
+            return {'skip': next_page_token.get('skip'), 'limit': self.limit}
+        else:
+            return {'limit': self.limit}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
@@ -82,24 +90,21 @@ class JumpcloudStream(HttpStream, ABC):
         """
         yield from response.json().get("results")
 
-
-class Customers(JumpcloudStream):
+class SystemUsers(JumpcloudStream):
     """
-    TODO: Change class name to match the table/data source this stream corresponds to.
+    V1 SystemUsers API.
     """
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+    
+    primary_key = "_id"
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
         should return "customers". Required.
         """
-        return "customers"
-
+        return "systemusers/"
 
 # Basic incremental stream
 class IncrementalJumpcloudStream(JumpcloudStream, ABC):
@@ -127,48 +132,6 @@ class IncrementalJumpcloudStream(JumpcloudStream, ABC):
         """
         return {}
 
-
-class Employees(IncrementalJumpcloudStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
-
 # Source
 class SourceJumpcloud(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
@@ -191,5 +154,5 @@ class SourceJumpcloud(AbstractSource):
         """
         auth = TokenAuthenticator(token=config.get("api_key"), auth_header="x-api-key")
         return [
-            Employees(authenticator=auth)
+            SystemUsers(authenticator=auth)
         ]
