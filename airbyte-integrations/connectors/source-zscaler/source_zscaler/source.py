f15ff8c0553317a7a55c12948d6250ef3dd81c8d
Enrico Tuvera Jr <enrico.tuvera@infinit-o.com>
2022-04-12 18:31:53 +0800

    Got AUTH working
    - AdminUsers technically works, in that it also returns the same error code as postman: 403.
    - added catalogs for dev purposes

--- airbyte-integrations/connectors/source-zscaler/source_zscaler/source.py
+++ airbyte-integrations/connectors/source-zscaler/source_zscaler/source.py
@@ -5,6 +5,7 @@ from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 from urllib.parse import urljoin
 import time
+import json
 
 import requests
 from airbyte_cdk.sources import AbstractSource
@@ -55,29 +56,31 @@ class ZscalerStream(HttpStream, ABC):
     See the reference docs for the full list of configurable options.
     """
 
-    # TODO: Fill in the url base. Required.
-    # https://zsapi.zscaler.net/api/v1/authenticatedSession
-
     @property
     def url_base(self):
+        # EXAMPLE: https://zsapi.zscaler.net/api/v1/authenticatedSession
         return urljoin(self.config["base_url"], "/api/v1/")
 
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.config = config
 
         # Zscaler API requires that the api key be obfuscated.
         obfuscated = self.obfuscateApiKey(self.config["api_key"])
         login_body = {
             "username": self.config["username"],
             "password": self.config["password"],
-            "api_key": obfuscated["obfuscated_key"],
+            "apiKey": obfuscated["obfuscated_key"],
             "timestamp": obfuscated["timestamp"]
         }
-        login_url = urljoin(self.url_base, "authenticatedSession/")
+        login_url = urljoin(self.url_base, "authenticatedSession")
         # NOTE: This should handle auth for the whole run, as Requests
         #       will automatically track and send cookies with each request 
         #       after this one.
-        self._session.post(url=login_url, json=login_body)
+        resp = self._session.post(url=login_url, json=login_body)
+        # NOTE: Maybe raise_for_status() here?
+        resp.raise_for_status()
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
@@ -113,6 +116,14 @@ class ZscalerStream(HttpStream, ABC):
         yield {}
 
     def obfuscateApiKey(self, seed) -> Mapping[str, any]:
+        """
+        The Zscaler API requires that API keys be "obfuscated" in this manner. This snippet
+        of code was taken straight from their documentation.
+
+        :seed - str, represents the raw, unmodified API key from the admin console.
+
+        returns a dictionary containing a timestamp and the obfuscated key.
+        """
         now = int(time.time() * 1000)
         n = str(now)[-6:]
         r = str(int(n) >> 1).zfill(6)
@@ -127,94 +138,13 @@ class ZscalerStream(HttpStream, ABC):
             "obfuscated_key": key
         }
 
-
-class Customers(ZscalerStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
-
-    def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
-        should return "customers". Required.
-        """
-        return "customers"
-
-
-# Basic incremental stream
-class IncrementalZscalerStream(ZscalerStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
-
+class AdminUsers(ZscalerStream):
     @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+    def primary_key(self):
+        return "id"
 
-        :return str: The name of the cursor field.
-        """
-        return []
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
-
-
-class Employees(IncrementalZscalerStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
+    def path(self, stream_state, stream_slice, next_page_token):
+        return "adminUsers"
 
 # Source
 class SourceZscaler(AbstractSource):
@@ -237,6 +167,4 @@ class SourceZscaler(AbstractSource):
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        return [AdminUsers(config=config)]
