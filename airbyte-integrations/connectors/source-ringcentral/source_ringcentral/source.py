98b09878cfd8c5d4f6907d6dc05a3b0c6f750494
Enrico Tuvera Jr <enrico.tuvera@infinit-o.com>
2022-11-15 20:27:51 +0800

    company directory and extensions are working, need account permissions for the rest

--- airbyte-integrations/connectors/source-ringcentral/source_ringcentral/source.py
+++ airbyte-integrations/connectors/source-ringcentral/source_ringcentral/source.py
@@ -5,16 +5,20 @@
 
 from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from urllib.parse import urlparse
+from urllib.parse import parse_qs 
 
 import requests
+import ringcentral
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from ringcentral.platform import Platform
+from airbyte_cdk.models import SyncMode
+
+from ringcentral.http.client import Client
 
 """
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
 This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
 incremental syncs from an HTTP API.
 
@@ -26,12 +30,19 @@ The approach here is not authoritative, and devs are free to use their own judge
 There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
 """
 
+# NOTE: platform.get() is the entry into the ringcentral API
+# NOTE: need to find where I Can just drop this in to get it to work
+# NOTE: MEO is as follows 
+#   = read_records() 
+#   s-> self.request_headers() 
+#   -> self._create_prepared_request() 
+#   -> self.request_kwargs()
+#   -> self._send_request()
+# Lots to cover I guess
 
 # Basic full refresh stream
 class RingcentralStream(HttpStream, ABC):
     """
-    TODO remove this comment
-
     This class represents a stream output by the connector.
     This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
     parsing responses etc..
@@ -55,13 +66,15 @@ class RingcentralStream(HttpStream, ABC):
     See the reference docs for the full list of configurable options.
     """
 
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
+    # NOTE: This isn't strictly necessary anymore.
+    url_base = ""
+
+    def __init__(self, platform: Platform, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._platform = platform
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
         This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
         to most other methods in this class to help you form headers, request bodies, query params, etc..
 
@@ -73,119 +86,115 @@ class RingcentralStream(HttpStream, ABC):
         :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
                 If there are no more pages in the result, return None.
         """
-        return None
+        rjson = response.json()
+        paging = rjson["paging"]
+
+        current_page = int(paging["page"])
+        last_page = int(paging["pageEnd"])
+
+        if current_page < last_page:
+            return {"next_page": int(current_page) + 1}
+        else:
+            return None
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
         Usually contains common params e.g. pagination size etc.
         """
-        return {}
+        if next_page_token:
+            return {"page": next_page_token["page"]}
+        else:
+            return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
-        TODO: Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        yield {}
+        rjson = response.json()
+        yield from rjson["records"]
+
+    def read_records(
+        self,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping[str, Any]]:
+        next_page_token = None
+        pagination_complete = False
+        while not pagination_complete:
+            path = self.path(
+                stream_state=stream_state,
+                stream_slice=stream_slice,
+                next_page_token=next_page_token
+            )
+
+            raw_response = self._platform.get(path)
+            response = raw_response._response
+
+            yield from self.parse_response(response)
+
+            next_page_token = self.next_page_token(response)
+
+            if not next_page_token:
+                pagination_complete = True
 
 
-class Customers(RingcentralStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+class Extensions(RingcentralStream):
+    primary_key = "id"
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
         should return "customers". Required.
         """
-        return "customers"
+        return "/account/~/extension"
 
+class CallLog(RingcentralStream):
+    primary_key = "id"
 
-# Basic incremental stream
-class IncrementalRingcentralStream(RingcentralStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
-
-    @property
-    def cursor_field(self) -> str:
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
         """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
         """
-        return []
+        return "/account/~/call-log"
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+class CompanyDirectory(RingcentralStream):
+    primary_key = "id"
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
         """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
         """
-        return {}
-
-
-class Employees(IncrementalRingcentralStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
+        return "/account/~/directory/entries"
 
 # Source
 class SourceRingcentral(AbstractSource):
+
+    def _create_rc_platform(self, client_id: str, client_secret: str, client_server: str, jwt: str):
+        sdk: ringcentral.SDK = ringcentral.SDK(
+            client_id,
+            client_secret,
+            client_server,
+        )
+        platform: ringcentral.platform.Platform = sdk.platform()
+        # NOTE: This already does the raise_for_status() inside
+        platform.login(jwt=jwt)
+
+        return platform
+
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
-        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
-
         See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
         for an example.
 
@@ -193,14 +202,45 @@ class SourceRingcentral(AbstractSource):
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
+        client_id: str = config["rc_client_id"]
+        client_secret: str = config["rc_client_secret"]
+        server_url: str = config["rc_server_url"]
+        jwt: str = config["rc_jwt"]
+
+        try:
+            platform: ringcentral.platform.Platform = self._create_rc_platform(
+                client_id=client_id,
+                client_secret=client_secret,
+                client_server=server_url,
+                jwt=jwt
+            )
+        except requests.exceptions.HTTPError as e:
+            logger.info("Could not log in with credentials provided!")
+            logger.info("Check input and try again!")
+            logger.debug(f"Exception: {e}")
+            logger.debug(f"client_id: {client_id}")
+            logger.debug(f"client_secret: {client_secret}")
+            logger.debug(f"server_url: {server_url}")
+            logger.debug(f"jwt: {jwt}")
+            return False, "failed to log in."
+
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
-        TODO: Replace the streams below with your own streams.
-
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        client_id: str = config["rc_client_id"]
+        client_secret: str = config["rc_client_secret"]
+        server_url: str = config["rc_server_url"]
+        jwt: str = config["rc_jwt"]
+        platform: ringcentral.platform.Platform = self._create_rc_platform(
+            client_id,
+            client_secret,
+            server_url,
+            jwt,
+        )
+        return [
+            Extensions(platform=platform),
+            CompanyDirectory(platform=platform)
+        ]
