a7069d71117367904b2227e05eadf8b65e35f764
Enrico Tuvera Jr <enrico.tuvera@infinit-o.com>
2023-01-17 13:08:08 +0800

    first; and last commit for now

---
+++ airbyte-integrations/connectors/source-phished-io/source_phished_io/source.py
@@ -0,0 +1,158 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+from abc import ABC
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+
+import requests
+from requests.auth import AuthBase
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.http import HttpStream
+# from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
+
+"""
+This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
+incremental syncs from an HTTP API.
+
+The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
+stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
+
+The approach here is not authoritative, and devs are free to use their own judgement.
+
+There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
+"""
+
+
+# Basic full refresh stream
+class PhishedIoStream(HttpStream, ABC):
+    """
+    This class represents a stream output by the connector.
+    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
+    parsing responses etc..
+
+    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
+
+    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
+    contains the endpoints
+        - GET v1/customers
+        - GET v1/employees
+
+    then you should have three classes:
+    `class PhishedIoStream(HttpStream, ABC)` which is the current class
+    `class Customers(PhishedIoStream)` contains behavior to pull data for customers using v1/customers
+    `class Employees(PhishedIoStream)` contains behavior to pull data for employees using v1/employees
+
+    If some streams implement incremental sync, it is typical to create another class
+    `class IncrementalPhishedIoStream((PhishedIoStream), ABC)` then have concrete stream implementations extend it. An example
+    is provided below.
+
+    See the reference docs for the full list of configurable options.
+    """
+
+    # TODO: Fill in the url base. Required.
+    url_base = "https://app-v2.phished.io"
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        """
+        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
+        to most other methods in this class to help you form headers, request bodies, query params, etc..
+
+        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
+        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
+        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
+
+        :param response: the most recent response from the API
+        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
+                If there are no more pages in the result, return None.
+        """
+        return None
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        """
+        Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
+        Usually contains common params e.g. pagination size etc.
+        """
+        return {}
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        """
+        Override this method to define how a response is parsed.
+        :return an iterable containing each record in the response
+        """
+        data = response.json()
+        yield from data
+
+
+class Events(PhishedIoStream):
+    primary_key = "Recipient_ID"
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        """
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
+        """
+        return "/api/tasks/powerbi/events"
+
+class QueryParamAuthenticator(AuthBase):
+    def __init__(self, organization_id: str, secret: str, integration_id: str):
+        self.org_id = organization_id
+        self.secret = secret
+        self.int_id = integration_id
+
+    def __call__(self, r: requests.Request):
+        url: str = r.url
+        r.prepare_url(url, params={
+            "organisationid": self.org_id,
+            "secret": self.secret,
+            "integrationid": self.int_id
+        })
+
+        return r
+
+# Source
+class SourcePhishedIo(AbstractSource):
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        """
+        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
+
+        See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
+        for an example.
+
+        :param config:  the user-input config object conforming to the connector's spec.yaml
+        :param logger:  logger object
+        :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
+        """
+        test_url = "https://app-v2.phished.io/api/tasks/powerbi/events"
+        test_url = test_url.format(config["organization_id"], config["secret"], config["integration_id"])
+
+        r = requests.get(test_url, params={
+            "organisationid": config["organization_id"],
+            "secret": config["secret"],
+            "integrationid": config["integration_id"]
+        })
+
+        if r.status_code != 200:
+            return False, "Credentials invalid, check and try again!"
+        else:
+            return True, None
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        """
+        Replace the streams below with your own streams.
+
+        :param config: A Mapping of the user input configuration as defined in the connector spec.
+        """
+        auth: QueryParamAuthenticator = QueryParamAuthenticator(
+            organization_id=config["organization_id"],
+            secret=config["secret"],
+            integration_id=config["integration_id"]
+        )
+        return [Events(authenticator=auth)]
