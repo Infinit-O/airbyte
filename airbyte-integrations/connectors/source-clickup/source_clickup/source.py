e9910e506acadf484d90d9eef1b75ccc9c96ee09
Enrico Tuvera Jr <enrico.tuvera@infinit-o.com>
2023-03-06 22:18:11 +0800

    nearly everything done; ive hit the rate limit so im calling it a night

--- airbyte-integrations/connectors/source-clickup/source_clickup/source.py
+++ airbyte-integrations/connectors/source-clickup/source_clickup/source.py
@@ -7,14 +7,13 @@ from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
+from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
 """
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
 This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
 incremental syncs from an HTTP API.
 
@@ -26,12 +25,19 @@ The approach here is not authoritative, and devs are free to use their own judge
 There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
 """
 
+class ReplacementTokenAuthenticator(TokenAuthenticator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    # NOTE: I know that this is deprecated but this is the fastest
+    #       way to do things.
+    def get_auth_header(self) -> Mapping[str, Any]:
+        return {self.auth_header: f"{self.auth_method}{self._token}"}
+
 
 # Basic full refresh stream
 class ClickupStream(HttpStream, ABC):
     """
-    TODO remove this comment
-
     This class represents a stream output by the connector.
     This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
     parsing responses etc..
@@ -55,13 +61,14 @@ class ClickupStream(HttpStream, ABC):
     See the reference docs for the full list of configurable options.
     """
 
-    # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
+    url_base = "https://api.clickup.com/api/v2/"
+
+    @property
+    def entity_name(self):
+        raise NotImplementedError
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
         This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
         to most other methods in this class to help you form headers, request bodies, query params, etc..
 
@@ -79,112 +86,124 @@ class ClickupStream(HttpStream, ABC):
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
+        Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
         Usually contains common params e.g. pagination size etc.
         """
         return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
-        TODO: Override this method to define how a response is parsed.
+        Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        yield {}
+        data = response.json()
+        contents = data[self.entity_name]
+        yield from contents
 
+class ClickupSubStream(ClickupStream):
+    @property
+    def parent(self):
+        raise NotImplementedError
 
-class Customers(ClickupStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+    def stream_slices(
+        self, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
+    ) -> Iterable[Optional[Mapping[str, Any]]]:
+        # parent_stream_slices = self.parent.stream_slices(
+        #     sync_mode=SyncMode.full_refresh, cursor_field=cursor_field, stream_state=stream_state
+        # )
+        parent = self.parent(authenticator=self.authenticator)
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+        parent_slices = parent.stream_slices(
+            sync_mode=SyncMode.full_refresh, cursor_field=cursor_field, stream_state=stream_state
+        )
+
+        # iterate over all parent stream_slices
+        # for stream_slice in parent_stream_slices:
+        for slice in parent_slices:
+            for record in parent.read_records(
+                sync_mode=SyncMode.full_refresh, cursor_field=cursor_field, stream_state=stream_state,
+                stream_slice=slice
+            ):
+                yield {"parent": record}
+
+class Teams(ClickupStream):
+    primary_key = "id"
+    entity_name = "teams"
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
         """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
         should return "customers". Required.
         """
-        return "customers"
+        return "team/"
 
+class Spaces(ClickupSubStream):
+    primary_key = "id"
+    parent = Teams
+    entity_name= "spaces"
 
-# Basic incremental stream
-class IncrementalClickupStream(ClickupStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
-
-    @property
-    def cursor_field(self) -> str:
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
         """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
         """
-        return []
+        team_id = stream_slice["parent"]["id"]
+        return f"team/{team_id}/space"
+    
+class Folders(ClickupSubStream):
+    primary_key = "id"
+    parent = Spaces
+    entity_name = "folders"
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
         """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
         """
-        return {}
+        team_id = stream_slice["parent"]["id"]
+        return f"space/{team_id}/folder"
 
+class Lists(ClickupSubStream):
+    primary_key = "id"
+    parent = Folders
+    entity_name = "lists"
 
-class Employees(IncrementalClickupStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
         """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
         """
-        return "employees"
+        team_id = stream_slice["parent"]["id"]
+        return f"folder/{team_id}/list"
+    
+class Tasks(ClickupSubStream):
+    primary_key = "id"
+    parent = Lists
+    entity_name = "tasks"
 
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
         """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
+        Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
+        should return "customers". Required.
         """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
+        team_id = stream_slice["parent"]["id"]
+        return f"list/{team_id}/task"
 
 # Source
 class SourceClickup(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
-        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
+        Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
 
         See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
         for an example.
@@ -193,14 +212,23 @@ class SourceClickup(AbstractSource):
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
+        api_token = config["api_token"]
+        headers = {"Authorization": f"{api_token}"}
+        simple_request = requests.get("https://api.clickup.com/api/v2/team", headers=headers)
+        simple_request.raise_for_status()
+
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
-        TODO: Replace the streams below with your own streams.
-
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        api_key = config["api_token"]
+        auth = ReplacementTokenAuthenticator(token=api_key, auth_method="")  
+        return [
+            Teams(authenticator=auth),
+            Spaces(authenticator=auth),
+            Folders(authenticator=auth),
+            Lists(authenticator=auth),
+            Tasks(authenticator=auth),
+        ]
